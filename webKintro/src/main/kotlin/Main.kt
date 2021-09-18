3791bccaaa2fa2edfa4d617078e73ec8682b2f04
ybonnetain <yann.bonnetain@gmail.com>
2021-09-18 19:13:08 +0200

    tmp

--- webKintro/src/main/kotlin/Main.kt
+++ webKintro/src/main/kotlin/Main.kt
@@ -1,30 +1,35 @@
-//import kotlinx.coroutines.InternalCoroutinesApi
-import dev.ybonnetain.kintro.di.initKoin
-import dev.ybonnetain.kintro.models.User
-import dev.ybonnetain.kintro.store.UsersAction
-import dev.ybonnetain.kintro.store.UsersState
-import dev.ybonnetain.kintro.store.UsersStore
-//import org.koin.core.KoinApplication.Companion.init
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
 
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
+import dev.ybonnetain.kintro.di.initKoin
+import dev.ybonnetain.kintro.store.TodosAction
+import dev.ybonnetain.kintro.store.TodosStore
+import dev.ybonnetain.kintro.store.UsersAction
+import dev.ybonnetain.kintro.store.UsersStore
+
 
 // https://discuss.kotlinlang.org/t/kotlin-js-1-4-how-to-output-a-npm-package/19128
-
 // https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.js/-js-export/
+
 object Shared : KoinComponent {
     val mainScope = MainScope()
 
-    val store : UsersStore
-    val load = UsersAction.Load
+    val store : TodosStore
+    val load = TodosAction.Load
 
-    fun _dispatch(action: UsersAction) {
+    val dispatch = :: _dispatch
+    val observe = :: _observe
+
+    init {
+        initKoin()
+        store = get()
+    }
+
+    fun _dispatch(action: TodosAction) {
         store.dispatch(action)
     }
 
@@ -35,25 +40,10 @@ object Shared : KoinComponent {
             }
         }
     }
-
-    val dispatch = :: _dispatch
-
-    val observe = :: _observe
-
-    init {
-        initKoin()
-        store = get()
-    }
 }
 
-
-//@InternalCoroutinesApi
 fun main() {
+    // make the shared module available directly after main invocation
     console.log("Shared main IIFE")
     Shared
-
-//    app.store.observeState()
-//    app.store.dispatch(app.load)
 }
-
-
