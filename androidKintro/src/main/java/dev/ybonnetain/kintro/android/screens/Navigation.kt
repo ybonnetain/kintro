3ee1738780f06da0022660c5bc5748e906dc3efe
Yann Bonnetain <yann.bonnetain@gmail.com>
2021-10-31 19:37:52 +0100

    complete todos screen (#22)

--- androidKintro/src/main/java/dev/ybonnetain/kintro/android/screens/Navigation.kt
+++ androidKintro/src/main/java/dev/ybonnetain/kintro/android/screens/Navigation.kt
@@ -1,6 +1,5 @@
 package dev.ybonnetain.kintro.android.screens
 
-import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.CheckCircle
